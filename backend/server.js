6f9f1e99de17d98439ae97b3dcd1c591d2a06ecc
justinjgh <justinjgh@gmail.com>
2020-11-09 09:56:43 -0600

    Just needs touch ups - submitted for grading

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import path from 'path';
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
+import orderRouter from "./routers/orderRouter.js";
+import uploadRouter from "./routers/uploadRouter.js";
 
 dotenv.config();
 
@@ -20,22 +23,20 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/reptizon", {
   useCreateIndex: true,
 });
 
-// app.get("/api/products/:id", (req, res) => {
-//   const product = data.products.find((x) => x._id === req.params.id);
-//   if (product) {
-//     res.send(product);
-//   } else {
-//     res.status(404).send({ message: "Product not found" });
-//   }
-// });
-
-// app.get("/api/products", (req, res) => {
-//   res.send(data.products);
-// }); This was used when data was coming in staticly from data.js - now coming from MONGO
+app.use('/api/uploads', uploadRouter);
 
 app.use("/api/users", userRouter);
 
 app.use('/api/products', productRouter);
+
+app.use('/api/orders', orderRouter);
+
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
@@ -49,3 +50,21 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is up at http://localhost:${port}`);
 });
+
+
+
+
+
+
+// app.get("/api/products/:id", (req, res) => {
+//   const product = data.products.find((x) => x._id === req.params.id);
+//   if (product) {
+//     res.send(product);
+//   } else {
+//     res.status(404).send({ message: "Product not found" });
+//   }
+// });
+
+// app.get("/api/products", (req, res) => {
+//   res.send(data.products);
+// }); This was used when data was coming in staticly from data.js - now coming from MONGO
